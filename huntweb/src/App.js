24e3b600142a65a0e55b5b94ead71edab980fa8f
medson mendes <medson-mbp@hotmail.com>
2019-03-07 14:23:00 -0300

    add a simple react app

---
+++ huntweb/src/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Routes from "./routes";
+
+import Main from "./pages/main";
+
+const App = () => <div className="App">{<Routes />}</div>;
+export default App;
